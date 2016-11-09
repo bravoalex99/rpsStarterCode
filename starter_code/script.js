9d9c8d0aca797554e3352f7aa919a776990df756
Alexis Bravo <bravoalexis99@gmail.com>
2016-11-09 23:32:06 +0000

    rock paper and scissors

--- starter_code/script.js
+++ starter_code/script.js
@@ -21,43 +21,82 @@ var pickWinner = function(userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-    
+    var Choice;
+
+
     if (userChoice === computerChoice) {
-    console.log("tie");
+        Choice = "tie";
+        console.log("tie");
     }
-    else if (userChoice == "paper" && computerChoice == "rock"){
+    else if (userChoice == "paper" && computerChoice == "rock") {
+        Choice = "user wins";;
         console.log("user wins");
     }
-    else if (userChoice == "paper" && computerChoice == "scissors"){
+    else if (userChoice == "paper" && computerChoice == "scissors") {
+        Choice = "computer wins";
         console.log("computer wins");
     }
-    else if (userChoice == "scissors" && computerChoice == "paper"){
+    else if (userChoice == "scissors" && computerChoice == "paper") {
+        Choice = "user win";
         console.log("user wins");
     }
-    else if (userChoice == "scissors" && computerChoice == "rock"){
+    else if (userChoice == "scissors" && computerChoice == "rock") {
+        Choice = "computer wins";
         console.log("computer wins");
     }
-    else if (userChoice == "rock" && computerChoice == "scissors"){
+    else if (userChoice == "rock" && computerChoice == "scissors") {
+        Choice = "user wins";
         console.log("user wins");
     }
-    else if (userChoice == "rock" && computerChoice == "paper"){
+    else if (userChoice == "rock" && computerChoice == "paper") {
+        Choice = "computer wins";
         console.log("computer wins");
     }
-    
-    
-    
-    
-    
+
+
+
+
+
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
 
     // Task 4: Show `computerChoice` in HTML after the words "Computer's choice:"
+    $("#computer-choice").html(computerChoice);
+
+    if (computerChoice === "rock") {
+        $("#computer-choice").html('<img id="rock" src=http://images.clipartpanda.com/rocks-clipart-rock-clipart.png width="100px" />');
+
+    }
+    else if (computerChoice === "paper") {
+        $("#computer-choice").html('<img id="paper" src=http://clipartix.com/wp-content/uploads/2016/04/Paper-clip-art-free-free-clipart-images-2-clipartcow.png width="100px" />');
+    }
+else if (computerChoice ==="scissors"){
+        $("#computer-choice").html('<img id="scissors" src=http://images.clipartpanda.com/scissors-clip-art-1388423321.png width="100px" />');
+
+}
     // Task 5: Show the winner in HTML after the word "Winner:"
+    $("#winner").html(Choice);
 };
 
 /* DOCUMENT READY: Everything inside this function will happen after
    the user's browser has finished loading the webpage. */
 $(document).ready(function() {
+
+    $("#rock").click(function() {
+        var userChoice = "rock";
+        var computerChoice = generateComputerChoice();
+        pickWinner(userChoice, computerChoice);
+    });
+    $("#paper").click(function() {
+        var userChoice = "paper";
+        var computerChoice = generateComputerChoice();
+        pickWinner(userChoice, computerChoice);
+    });
+    $("#scissors").click(function() {
+        var userChoice = "scissors";
+        var computerChoice = generateComputerChoice();
+        pickWinner(userChoice, computerChoice);
+    });
 
     // This line calls the `generateComputerChoice` function and assigns its
     // return value to the variable `computerChoice`.
